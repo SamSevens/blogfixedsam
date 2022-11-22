5df4dbdd38cde95f18a0c0730e1baef8dadb3cf6
samsevens <samrosemount@gmail.com>
2022-11-21 22:01:45 -0500

    topic bar fixed and ordered

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 # blog/views.py
 
 from django.shortcuts import render
+from django.db.models import Count
+
 from . import models
 
 
@@ -10,7 +12,7 @@ def home(request):
     """
     latest_posts = models.Post.objects.published().order_by('-published')[:3]
     authors = models.Post.objects.get_authors()
-    topics = models.Topic.objects.all()[:10]
+    topics = models.Topic.objects.all().annotate(c=Count('blog_posts')).order_by('-c')[:10]
     context = {
         'authors': authors,
         'latest_posts': latest_posts,
