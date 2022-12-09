e36a37b76ee54ea165ddd915f92a4618da31cde9
samsevens <samrosemount@gmail.com>
2022-12-09 18:18:51 -0500

    fixed ip address

--- blogfixedsam/settings.py
+++ blogfixedsam/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-2ickrndt9ywp2x+po-a*60n02*9fn%f!4_pt@vx26#x4l3!k3z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', '1'))
 
-ALLOWED_HOSTS = ['198.211.110.79', '10.116.0.2']
+ALLOWED_HOSTS = ['198.211.110.79']
 
 
 # Application definition
@@ -130,6 +131,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+CKEDITOR_UPLOAD_PATH = 'uploads/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
