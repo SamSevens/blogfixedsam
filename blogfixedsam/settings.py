5644709530dae881979248a6855cc33aee448188
samsevens <samrosemount@gmail.com>
2022-12-09 16:37:26 -0500

    fixing settings for database

--- blogfixedsam/settings.py
+++ blogfixedsam/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'blogfixedsam',
-        'USER': 'sam',
-        'PASSWORD': 'postgres',
+        'USER': 'instructor',
+        'PASSWORD': 'DjangoRocks1!',
         'HOST': 'localhost',
         'PORT': '',
     }
