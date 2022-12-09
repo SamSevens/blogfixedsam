9321492619616f627b7929aff4a1b9607fe9f895
samsevens <samrosemount@gmail.com>
2022-12-09 17:50:09 -0500

    fixing settings

--- blogfixedsam/settings.py
+++ blogfixedsam/settings.py
@@ -130,7 +130,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-CKEDITOR_UPLOAD_PATH = 'uploads/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
